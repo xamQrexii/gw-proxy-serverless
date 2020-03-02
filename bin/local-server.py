f28558f7f18fc47157ddfe4adb6fbde4f07a0086
Andriy Tykhonov <atykhonov@gmail.com>
2020-03-02 04:39:17 +0200

    Fix submodule fetching.

--- bin/local-server.py
+++ bin/local-server.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python
+
+import os
 import sys
-sys.path.append('.')
-sys.path.append('./modules/OSBOT-Utils')
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
+MODULES_PATH = os.path.join(PROJECT_PATH, 'modules')
+
+sys.path.append(PROJECT_PATH)
+sys.path.append(os.path.join(MODULES_PATH, 'OSBot-Utils'))
+
 from gw_proxy.local_proxy.Server import Server
 
 print("***** starting local server")
