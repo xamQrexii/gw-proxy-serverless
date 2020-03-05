94843f422cd297c5b43b0e94de971394527b0ba6
xamqrexii <usama1630@gmail.com>
2020-03-06 00:11:54 +0500

    Updates

---
+++ precache-manifest.d85868c411298365ba5a1e86c5c4e97d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89797d9c39cf8da8926c6caa7677aac3",
+    "url": "/wirefreame-6/index.html"
+  },
+  {
+    "revision": "3590d19191125d3f17e3",
+    "url": "/wirefreame-6/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "6e672724ef6e2b06f054",
+    "url": "/wirefreame-6/static/css/main.039a4489.chunk.css"
+  },
+  {
+    "revision": "3590d19191125d3f17e3",
+    "url": "/wirefreame-6/static/js/2.b87615a6.chunk.js"
+  },
+  {
+    "revision": "e6e659ede2983e3ba57911926b8d9c10",
+    "url": "/wirefreame-6/static/js/2.b87615a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e672724ef6e2b06f054",
+    "url": "/wirefreame-6/static/js/main.d6a7595e.chunk.js"
+  },
+  {
+    "revision": "e7bcbf84671980aedd6a",
+    "url": "/wirefreame-6/static/js/runtime-main.44dfbcf2.js"
+  }
+]);
